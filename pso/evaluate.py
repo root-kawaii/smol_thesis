81b27df0891a1aa05a4acc886f9d82bf37c72243
root-kawaii <teo.regge.99@gmail.com>
2024-01-31 18:55:35 +0100

    latex init kinda

--- pso/evaluate.py
+++ pso/evaluate.py
@@ -30,6 +30,8 @@ animal_num = animal_num + "/"
 patho = "../pso/data/animal "
 folders = [f for f in os.listdir(patho + str(animal_num))]
 
+# plt.switch_backend('TkAgg')
+
 # files_nev = [f for f in os.listdir("../pso/data_nev")]
 data_list = []
 # other_list = []
@@ -55,15 +57,32 @@ for l in range(100):
 
             num_electrodes = 0
             # num_sensors = 0
+            # their code #################
+            # It's dealing with files that Elisa's old code generated, so we have to adequate it
+            # sample=sample[0,:,0:16]
+            # their code #################
             raw = mne.io.read_raw_nsx(
                 patho + str(animal_num) + fil + "/" + file, preload=True)
+            raw.describe()
+            sfreq = 100
             # apply a highpass filter from 1 Hz upwards
             # replace baselining with high-pass
             # test divide by 4
             # raw = raw/4
+            # raw._data *= 1e+3
+            # raw.filter(l_freq=800, h_freq=None)
             raw.filter(l_freq=800, h_freq=2500)
-            sfreq = 64
+
             raw.resample(5000)
+            raw.drop_channels(["flexsensor", "pressuresensor", "motors"])
+            # raw.compute_psd(fmax=50).plot(
+            #     picks="data", exclude="bads", amplitude=False)
+
+            # ica = mne.preprocessing.ICA(
+            #     n_components=4, random_state=97, max_iter=800)
+            # ica.fit(raw)
+            # raw.load_data()
+            # ica.apply(raw)
 
             time_on = 0.5  # definiti dal sensore codice davide da vedere, cambiano da animale ad animale e da classe a classe
             time_off = 4  # definiti del sensore
@@ -76,8 +95,8 @@ for l in range(100):
             for j in range(len(x)):
                 if (position[j] != 1 and j < 16):
                     rows_to_del.append(j)
-                if (j > 15):
-                    rows_to_del.append(j)
+                # if (j > 15):
+                #     rows_to_del.append(j)
                 elif (position[j] == 1 and j < 16):
                     num_electrodes += 1
                 # elif (j > 15):
@@ -86,13 +105,25 @@ for l in range(100):
             print(len(x[1]))
             x = np.delete(x, rows_to_del, 0)
 
-            for t in range(len(x)):
-                for y in range(len(x[t])):
-                    # print(x[t, y])
-                    # x[t, y] = int(x[t, y] / 4)
-                    if (abs(x[t, y]) > 0.0000030):
-                        counter += 1
-                        x[t, y] = 0.0000030
+            # fig, axd = plt.subplot_mosaic(
+            #     [["image", "density"],
+            #      ["EEG", "EEG"]],
+            #     layout="constrained",
+            #     # "image" will contain a square image. We fine-tune the width so that
+            #     # there is no excess horizontal or vertical margin around the image.
+            #     width_ratios=[1.05, 2],
+            # )
+            # for i in range(16):
+            #     plt.plot(x[i])
+            #     plt.show()
+
+            # for t in range(len(x)):
+            #     for y in range(len(x[t])):
+            #         # print(x[t, y])
+            #         # x[t, y] = int(x[t, y] / 4)
+            #         if (abs(x[t, y]) > 0.0000030):
+            #             counter += 1
+            #             x[t, y] = 0.0000030
 
             print(counter)
             # for t in range(len(x)):
@@ -110,6 +141,7 @@ for l in range(100):
             #             x[t, y] = 0
 
             print('cleaned')
+
             # create a numpy array of EEG data from the MNE raw object
             # eeg_array = x[:, 0:len(x[0])]
 
